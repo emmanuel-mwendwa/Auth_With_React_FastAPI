f81ea714a2617b07315a1bf0eba465ce658a84bc
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-03-28 17:53:02 +0300

    Register page

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -1,8 +1,124 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Flex, Typography, Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+
+import registerImage from "../assets/register.jpg";
 
 const Register = () => {
-  return <Card>Register</Card>;
+  const handleRegister = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Card className="form-container">
+      <Flex gap="large" align="center">
+        {/* form  */}
+        <Flex vertical flex={1}>
+          <Typography.Title level={3} strong className="title">
+            Create an account
+          </Typography.Title>
+          <Typography.Text type="secondary" strong className="slogan">
+            Join for exclusive access!
+          </Typography.Text>
+          <Form layout="vertical" onFinish={handleRegister} autoComplete="off">
+            <Form.Item
+              label="First Name"
+              name="first_name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your first name!",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your first name" />
+            </Form.Item>
+
+            <Form.Item
+              label="Last Name"
+              name="last_name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your last name!",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your last name" />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+                {
+                  type: "email",
+                  message: "The input is not a valid Email!",
+                },
+              ]}
+            >
+              <Input size="large" placeholder="Enter your email" />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Password!",
+                },
+              ]}
+            >
+              <Input.Password size="large" placeholder="Enter your password" />
+            </Form.Item>
+
+            <Form.Item
+              label="Confirm Password"
+              name="confirm_password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please confirm your password!",
+                },
+              ]}
+            >
+              <Input.Password
+                size="large"
+                placeholder="Confirm your password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                size="large"
+                className="btn"
+              >
+                Create Account
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Link to="/login">
+                <Button size="large" className="btn">
+                  Sign In
+                </Button>
+              </Link>
+            </Form.Item>
+          </Form>
+        </Flex>
+        {/* image  */}
+        <Flex flex={1}>
+          <img src={registerImage} className="auth-image" />
+        </Flex>
+      </Flex>
+    </Card>
+  );
 };
 
 export default Register;
